cc2dce11ca1063bda32788180100498d653a287a
Will Harris <will.t.harris@protonmail.com>
2019-07-11 15:36:20 -0600

    add more structure to milestone-1 page for content to be added

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,6 +5,14 @@
 		<title>PWP Milestone 1</title>
 	</head>
 	<body>
-		<h1>Test</h1>
+		<h1>PWP Milestone 1</h1>
+		<h2>Purpose</h2>
+		<p>Purpose goes here</p>
+		<h2>Audience</h2>
+		<p>Audience goes here</p>
+		<h2>Goal</h2>
+		<p>Goal goes here</p>
+		<h2>Persona</h2>
+		<p>Persona goes here</p>
 	</body>
 </html>
