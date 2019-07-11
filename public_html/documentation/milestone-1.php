e333e0e56d98f3ffc7e202d227c88f769d2ecf54
Will Harris <will.t.harris@protonmail.com>
2019-07-11 16:55:06 -0600

    tweak purpose statment

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,7 +7,7 @@
 	<body>
 		<h1>PWP Milestone 1</h1>
 		<h2>Purpose</h2>
-		<p>I want my PWP to be a resource I can show potential employers to showcase my development skills in order to get a job as a Junior Developer.  I also think it will be helpful as a resource to show potential collaborators for independent projects (open source, small projects, etc)</p>
+		<p>I want my PWP to show potential employers my development skills in order to get a job as a Junior Developer.  I also want to use it as a resource to show potential collaborators for independent projects (open source, small projects, etc)</p>
 		<h2>Audience</h2>
 		<p>Potential employers and collaborators.</p>
 		<h2>Goal</h2>
