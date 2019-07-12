1d763b8875bc0de7a4fe78c8e7b2720c60154803
Will Harris <will.t.harris@protonmail.com>
2019-07-11 18:10:03 -0600

    start adding details for persona in milestone-1.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -14,6 +14,18 @@
 		<p>To acquire full-time employment as a Junior Developer. (Websites or any other web technology...I'd really like to work on privacy software/privacy focused web browsers(firefox, brave, duck duck go) eventually)</p>
 		<h2>Persona</h2>
 		<img src="bob-needadev.jpg" alt="image of bob needadev" />
+		<ul>
+			<li>Name: Bob Needadev</li>
+			<li>Age: 40</li>
+			<li>Company: NM Dev Lab</li>
+		</ul>
+		<h3>Needs/Wants to Find</h3>
+		<ul>
+			<li>Team-player with strong communication/inter-personal skills</li>
+			<li>Loyal young worker that is eager to learn, and who does not seem like they will jump ship in the first year of employment.</li>
+			<li>Dev with willingness to learn Python</li>
+			<li>* Dev with a strong grasp of VCS and a strong version control workflow</li>
+		</ul>
 		<p>Persona goes here</p>
 	</body>
 </html>
