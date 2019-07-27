692063249922a8846544f80baed852caa6c64ecd
Will Harris <will.t.harris@protonmail.com>
2019-07-27 14:16:12 -0600

    add sections for content strategy and the mobile/desktop site view

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,5 +6,13 @@
 </head>
 <body>
 	<h1>PWP Milestone 2</h1>
+
+	<h2>Content Strategy</h2>
+	<p></p>
+	<h2>Wireframes</h2>
+	<h3>Desktop View</h3>
+	<img />
+	<h3>Mobile View</h3>
+	<img />
 </body>
 </html>
