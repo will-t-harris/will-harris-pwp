cc6fdccfb637b6f92b95ab4cb36dd9c8054c65cc
Will Harris <will.t.harris@protonmail.com>
2019-07-29 17:47:05 -0600

    separate descriptions for welcome section on mobile and desktop

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,8 +8,10 @@
 	<h1>PWP Milestone 2</h1>
 
 	<h2>Content Strategy</h2>
-	<p><em>Welcome Page (desktop)</em> static left half of the desktop view with textured background.  Happy with how this looks so far, I think it gives a good degree of depth to the page.  Also want to include logos as links to my social media accounts in this section (github/linkedin/maybe instagram.  Want to include micro-animations for these logos from (useanimations.com)</p>
-	<p><em>Welcome Text:</em> a short introduction clearly stating my name and role as a web developer.  This welcome message will be overlaid on the textured background of my welcome page.  </p>
+	<p><em>Welcome Page (desktop):</em> fixed left half section of the desktop view with textured background.  Happy with how this looks so far, I think it gives a good degree of depth to the page.  Also want to include logos as links to my social media accounts in this section (github/linkedin/maybe instagram.  Want to include micro-animations for these logos from (useanimations.com)</p>
+	<p><em>Welcome Section (mobile):</em> I want to include the same texture and welcome message in the top third or so of the screen.  Social media images at top left with same micro-animations, if they will still work on mobile.</p>
+	<p>The right half of the desktop view I want to scroll while the left half remains static.  I want the texture effect to always be present on the site.</p>
+	<p><em>Welcome Text:</em> a short introduction clearly stating my name and role as a web developer.  This welcome message will be overlaid on the textured background of my welcome section.  </p>
 	<p><em>About Me:</em> Two short paragraphs describing my background/history and my current interests in technology/programming.</p>
 	<p><em>Get In Touch/Contact Form</em> </p>
 	<h2>Wireframes</h2>
