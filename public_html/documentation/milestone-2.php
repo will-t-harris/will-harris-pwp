ab5e789d7d4667b8dbfd2b6e812a900e9569f429
Will Harris <will.t.harris@protonmail.com>
2019-07-29 17:20:43 -0600

    add section describing welcome page for desktop view

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,9 +8,10 @@
 	<h1>PWP Milestone 2</h1>
 
 	<h2>Content Strategy</h2>
-	<p><em>Welcome Text:</em> a short introduction clearly stating my name and role as a web developer.  This welcome message will be overlaid on a textured background, below a small image/headshot of me.  </p>
+	<p><em>Welcome Page (desktop)</em> static left half of the desktop view with textured background.  Happy with how this looks so far, I think it gives a good degree of depth to the page.  Also want to include logos as links to my social media accounts in this section (github/linkedin/maybe instagram.  Want to include micro-animations for these logos from (useanimations.com)</p>
+	<p><em>Welcome Text:</em> a short introduction clearly stating my name and role as a web developer.  This welcome message will be overlaid on the textured background of my welcome page.  </p>
 	<p><em>About Me:</em> Two short paragraphs describing my background/history and my current interests in technology/programming.</p>
-
+	<p><em>Get In Touch/Contact Form</em> </p>
 	<h2>Wireframes</h2>
 	<h3>Desktop View</h3>
 	<img />
