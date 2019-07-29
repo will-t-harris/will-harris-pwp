7a050a9fe76ac7dd689843ebed315fc4ce08537e
Will Harris <will.t.harris@protonmail.com>
2019-07-29 17:08:10 -0600

    begin adding content strategy to pwp milestone

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,7 +8,9 @@
 	<h1>PWP Milestone 2</h1>
 
 	<h2>Content Strategy</h2>
-	<p></p>
+	<p><em>Welcome Text:</em> a short introduction clearly stating my name and role as a web developer.  This welcome message will be overlaid on a textured background, below a small image/headshot of me.  </p>
+	<p><em>About Me:</em> Two short paragraphs describing my background/history and my current interests in technology/programming.</p>
+
 	<h2>Wireframes</h2>
 	<h3>Desktop View</h3>
 	<img />
