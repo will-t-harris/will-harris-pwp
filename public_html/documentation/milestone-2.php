92f7dbb155d5c4aa2680bcaefbc047ffba49d975
Will Harris <will.t.harris@protonmail.com>
2019-08-03 17:40:11 -0600

    finish milestone-2, add index.php to documentation directory

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,12 +8,12 @@
 	<h1>PWP Milestone 2</h1>
 
 	<h2>Content Strategy</h2>
-	<p><strong>Welcome Section <em>(desktop view)</em>:</strong> fixed left half section of the desktop view with textured background.  Happy with how this looks so far, I think it gives a good degree of depth to the page.  Also want to include logos as links to my social media accounts in this section (github/linkedin/twitter/maybe instagram.  Want to include micro-animations for these logos from (useanimations.com)</p>
+	<p><strong>Welcome Section <em>(desktop view)</em>:</strong> fixed left half section of the desktop view with textured background.  Happy with how this looks so far, I think it gives a good degree of depth to the page.  Also want to include logos as links to my social media accounts in this section (github/linkedin/twitter/maybe instagram).  Want to include micro-animations for these logos from useanimations.com(need to look further into specific use of these, might not be able to use the files on linux)</p>
 	<p>The right half of the desktop view I want to scroll while the left half remains static.  I want the texture effect to always be present on the site.</p>
 	<p><strong>Welcome Section <em>(mobile view)</em>:</strong> I want to include the same texture and welcome message in the top third or so of the screen.  Social media images at top left with same micro-animations, if they will still work on mobile.</p>
 	<p><strong>Welcome Text:</strong> a short introduction clearly stating my name and role as a web developer.  This welcome message will be overlaid on the textured background of my welcome section, and will be directly below a small image of me (still waiting on a head-shot to come back via Dan).</p>
-	<p><strong>About Me:</strong> Two short paragraphs describing my background/history and my current interests in technology/programming. I want this to be short but compelling.</p>
-	<p><strong>Get In Touch/Contact Form:</strong></p>
+	<p><strong>About Me:</strong> Two short paragraphs describing my background/history and my current interests in technology/programming. I want this to be short but compelling.  Will also include a short description of the Deep Dive program to explain what we do concisely to potential employers.</p>
+	<p><strong>Get In Touch/Contact Form:</strong> bootstrap-styled contact form.  Might include nice icons from font-awesome for each input field.</p>
 	<p><strong>Footer Section:</strong> I want this section to have the same dark blue background and light gold color scheme and texture from the welcome sections.  Will include another set of social media icon links (on mobile view only) and copyright text.</p>
 	<h2>Wireframes</h2>
 	<h3>Mobile View</h3>
